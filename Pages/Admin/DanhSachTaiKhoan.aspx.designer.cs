af65f66b50de22957f5dd53969ab264cc0e01de2
hoang-chuc <hoang-chuc@users.noreply.github.com>
2026-07-20 08:03:03 +0700

    Fix designer.cs for DanhSachPhongBan and DanhSachTaiKhoan - add txtSearch and btnSearch declarations

--- Pages/Admin/DanhSachTaiKhoan.aspx.designer.cs
+++ Pages/Admin/DanhSachTaiKhoan.aspx.designer.cs
@@ -22,5 +22,15 @@ namespace QLNhanVien
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gvTaiKhoan;
+
+        /// <summary>
+        /// txtSearch control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.TextBox txtSearch;
+
+        /// <summary>
+        /// btnSearch control.
+        /// </summary>
+        protected global::System.Web.UI.WebControls.LinkButton btnSearch;
     }
 }
